e7b6190386afe8e04f069cdfcd69dfdd3aabbfcd
Danilo Righetto <danilo@tideal.com.br>
2020-05-03 20:56:42 -0300

    Adicionando componente com filhos - React

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
+// import Primeiro from './components/Primeiro'
+// import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render(
   <div>
-    <Primeiro></Primeiro>
+    <ComFilhos>
+      <ul>
+        <li>Ana</li>
+        <li>Carlos</li>
+        <li>Danilo</li>
+      </ul>
+    </ComFilhos>
+    {/* <Primeiro></Primeiro>
     <ComParametro 
       titulo="Esse é o titulo" 
-      subtitulo="Esse é o subtitulo" />
+      subtitulo="Esse é o subtitulo" /> */}
   </div>,
   document.getElementById('root')
 );
