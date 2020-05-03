1000bf20a79c2bb8091537e94446a6a41d0c583c
Danilo Righetto <danilo@tideal.com.br>
2020-05-03 10:41:59 -0300

    Adicionando novos componentes JSX - React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Primeiro></Primeiro>
+    <ComParametro 
+      titulo="Esse é o titulo" 
+      subtitulo="Esse é o subtitulo" />
+  </div>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
