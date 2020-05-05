a197521222f24aa3ed61f25739b7cb263d490b94
Danilo Righetto <danilo@tideal.com.br>
2020-05-05 10:21:06 -0300

    Ajustes no componente

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import React from 'react';
+
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
+
+export default (props) => (
+  <div className="App">
+    <Card titulo="#03 - Componente com Filhos">
+      <ComFilhos>
+        <ul>
+          <li>Ana</li>
+          <li>Carlos</li>
+          <li>Danilo</li>
+        </ul>
+      </ComFilhos>
+    </Card>
+    <Card titulo="#02 - Componente com Parametro">
+      <ComParametro
+        titulo="Esse é o titulo"
+        subtitulo="Esse é o subtitulo" />
+    </Card>
+    <Card titulo="#01 - Primeiro componente">
+      <Primeiro />
+    </Card>
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-// import Primeiro from './components/Primeiro'
-// import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
+import App from './App.jsx'
 
 ReactDOM.render(
-  <div>
-    <ComFilhos>
-      <ul>
-        <li>Ana</li>
-        <li>Carlos</li>
-        <li>Danilo</li>
-      </ul>
-    </ComFilhos>
-    {/* <Primeiro></Primeiro>
-    <ComParametro 
-      titulo="Esse é o titulo" 
-      subtitulo="Esse é o subtitulo" /> */}
-  </div>,
+  <App></App>,
   document.getElementById('root')
 );
